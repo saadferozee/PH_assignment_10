3178533bb65b260407a20df09762c094b483278f
saadferozee <saadferozee@gmail.com>
2026-01-03 02:03:31 +0600

    version-2.1.1, unmount React Typewriter, bug fixes

--- src/Elements/Headline.jsx
+++ src/Elements/Headline.jsx
@@ -8,7 +8,7 @@ const Headline = ({ typo }) => {
                 <span className="w-3 h-10 bg-[#556B2F] rounded-full"></span>
                 <span className="px-6 lg:px-22 py-2 bg-[#f7f3e9e5] border-4 border-[#556B2F] rounded-full shadow flex flex-col">
                     <span className='text-sm lg:text-3xl'>
-                        <Typewriter
+                        {/* <Typewriter
                             words={[typo]}
                             loop={0}
                             cursor
@@ -16,7 +16,8 @@ const Headline = ({ typo }) => {
                             typeSpeed={200}
                             deleteSpeed={40}
                             delaySpeed={2200}
-                        />
+                        /> */}
+                        {typo}
                     </span>
                 </span>
             </h1>

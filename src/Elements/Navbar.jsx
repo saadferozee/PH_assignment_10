44479d1cf01b5e3f6af9995c8b1945fb7f7db9a7
saadferozee <saadferozee@gmail.com>
2026-01-03 04:18:42 +0600

    version-2.2.1, navbar updated

--- src/Elements/Navbar.jsx
+++ src/Elements/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router';
 import AuthContext from '../Contexts/AuthContext';
-import { FaCloudMoon } from "react-icons/fa";
-import { FaCloudSun } from "react-icons/fa";
+import { FaCloudMoon, FaCloudSun } from "react-icons/fa";
+import { HiMenuAlt2 } from "react-icons/hi";
 import ReactTooltip from './ReactTooltip';
 
 
@@ -28,29 +28,36 @@ const Navbar = () => {
             }).catch(error => console.log(error));
     }
 
-    const links = <div className={`flex ${user ? 'gap-1' : 'gap-3'} font-stretch-125% text-[#F7F3E9] text-md`}>
-        <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/'}>Home</NavLink>
-        <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/listings'}>Pet & Supplies</NavLink>
+    const links = <div className={`flex ${user ? 'gap-1' : 'gap-3'} font-stretch-125% text-[#F7F3E9] text-xl`}>
+        <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/'}>Home</NavLink>
+        <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/listings'}>Pet & Supplies</NavLink>
         {
-            user && (
+            user ? (
                 <>
-                    <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/add-listing'}>Add Listing</NavLink>
-                    <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/my-listings'}>My Listings</NavLink>
-                    <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/my-orders'}>My Orders</NavLink>
+                    <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/add-listing'}>Add Listing</NavLink>
+                    <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/my-listings'}>My Listings</NavLink>
+                    <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/my-orders'}>My Orders</NavLink>
                 </>
+            ) : (
+                <div className='dropdown flex items-center'>
+                    <a tabIndex={0} role='button' className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent'>Policies</a>
+                    <ul tabIndex={-1} className="menu dropdown-content top-10 bg-[#556B2F40] rounded z-1 mt-3 w-fit p-2 shadow space-y-1.5">
+                        <NavLink className="px-4 pt-0.5 pb-0.75 rounded-full bg-[#F7F3E9] text-[#556B2F] cursor-pointer">Terms</NavLink>
+                        <NavLink className="px-4 pt-0.5 pb-0.75 rounded-full bg-[#F7F3E9] text-[#556B2F] cursor-pointer">Privacy</NavLink>
+                    </ul>
+                </div>
             )
         }
-        {/* <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/my-profile'}>My Profile</NavLink> */}
     </div>
     const linksDrop = <>
-        <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/'}>Home</NavLink>
-        <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/listings'}>Pet & Supplies</NavLink>
+        <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/'}>Home</NavLink>
+        <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/listings'}>Pet & Supplies</NavLink>
         {
             user && (
                 <>
-                    <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/add-listing'}>Add Listing</NavLink>
-                    <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/my-listings'}>My Listings</NavLink>
-                    <NavLink className='px-3 pt-1.5 pb-1.75 rounded-full border border-transparent' to={'/my-orders'}>My Orders</NavLink>
+                    <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/add-listing'}>Add Listing</NavLink>
+                    <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/my-listings'}>My Listings</NavLink>
+                    <NavLink className='px-3 pt-0.5 pb-0.75 rounded-full border border-transparent' to={'/my-orders'}>My Orders</NavLink>
                 </>
             )
         }
@@ -59,12 +66,12 @@ const Navbar = () => {
     return (
         <div className='sticky top-0 z-50'>
             <div className="bg-[#556B2F] shadow-lg shadow-[#00000040]">
-                <div className='navbar max-w-[1200px] mx-auto'>
+                <div className='navbar min-h-0 h-11 md:h-22 py-0  max-w-[1200px] mx-auto'>
                     <div className="navbar-start">
                         <ReactTooltip id={'nav'} content={'Click to Open Menu'} place={'bottom-start'}>
                             <div className="dropdown">
-                                <div tabIndex={0} role="button" className="mr-4 bt btn-ghos lg:hidden">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                                <div tabIndex={0} role="button" className="mr-3 md:mr-5 lg:hidden">
+                                    <HiMenuAlt2 className='text-[#F7F3E9] text-xl md:text-4xl' />
                                 </div>
                                 <ul
                                     tabIndex="-1"
@@ -76,7 +83,7 @@ const Navbar = () => {
                             </div>
                         </ReactTooltip>
                         <a href='/' className="flex items-center gap-2 pl-0 text-[#F7F3E9]">
-                            <img width={35} height={35} src="/Cat-logo.png" alt="cat" />
+                            <img className='w-6 md:w-10 h-fit' src="/Cat-logo.png" alt="cat" />
                             <span className='title hidden md:block font-light text-2xl'>AdoptyCo</span>
                         </a>
                     </div>
@@ -88,26 +95,33 @@ const Navbar = () => {
                         </ul>
                     </div>
                     <div className="navbar-end flex items-center gap-4">
-                        <button className='text-2xl text-[#F7F3E9] cursor-pointer' title='Click to change theme.' onClick={() => setDarkTheme(!darkTheme)}>{darkTheme ? <FaCloudMoon /> : <FaCloudSun />}</button>
+                        <button className='text-white text-xl md:text-4xl te0t-[#F703E9] cursor-pointer' title='Click to change theme.' onClick={() => setDarkTheme(!darkTheme)}>{darkTheme ? <FaCloudMoon /> : <FaCloudSun />}</button>
                         {
                             user ? (
                                 <div className='flex items-center gap-2'>
-                                    <a href="/my-profile">
-                                        {
-                                            user.photoURL ? (
-                                                <img title={`click to go Profile of User: ${user.displayName}`} className='w-10 h-10 object-cover border-2 border-[#F7F3E9] p-0.5 rounded-full' src={`${user.photoURL ? user.photoURL : 'https://img.icons8.com/ink/96/f7f3e9/user-male-circle.png'}`} alt="DP" />
-                                            ) : (
-                                                <img title={`click to go Profile of User: ${user.displayName}`} width={40} className='rounded-full' src={'https://img.icons8.com/ink/96/f7f3e9/user-male-circle.png'} alt="DP" />
-                                            )
-                                        }
-                                    </a>
-                                    <ReactTooltip id='logout' content={'Click to LogOut'} place={'bottom-end'}>
-                                        <button onClick={handleLogOutbutton} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] text-[#556B2F] cursor-pointer">LogOut</button>
-                                    </ReactTooltip>
+                                    <div className="dropdown">
+                                        <div tabIndex={0} role="button" >
+                                            {
+                                                user.photoURL ? (
+                                                    <img title={`click to go Profile of User: ${user.displayName}`} className='w-8 h-8 md:w-12 md:h-12 object-cover border-2 border-[#F7F3E9] p-0.5 rounded-full' src={`${user.photoURL ? user.photoURL : 'https://img.icons8.com/ink/96/f7f3e9/user-male-circle.png'}`} alt="DP" />
+                                                ) : (
+                                                    <img title={`click to go Profile of User: ${user.displayName}`} width={40} className='rounded-full' src={'https://img.icons8.com/ink/96/f7f3e9/user-male-circle.png'} alt="DP" />
+                                                )
+                                            }
+                                        </div>
+                                        <ul tabIndex={-1} className="menu dropdown-content right-1 bg-[#556B2F40] rounded z-1 mt-3 w-fit p-2 shadow space-y-1.5">
+                                            <ReactTooltip id='logout' content={'Click to see profile'} place={'bottom-end'}>
+                                                <button className="px-4 pt-0.5 pb-0.75 rounded-full bg-[#F7F3E9] text-[#556B2F] cursor-pointer">Profile</button>
+                                            </ReactTooltip>
+                                            <ReactTooltip id='logout' content={'Click to LogOut'} place={'bottom-end'}>
+                                                <button onClick={handleLogOutbutton} className="px-4 pt-0.5 pb-0.75 rounded-full bg-red-300 text-[#556B2F] cursor-pointer">LogOut</button>
+                                            </ReactTooltip>
+                                        </ul>
+                                    </div>
                                 </div>
                             ) : (
                                 <ReactTooltip id='login' content={'Click to Login'} place={'bottom-end'}>
-                                    <Link to={'/login'} className="px-4 pt-1.5 pb-1.75 rounded-full bg-[#F7F3E9] text-[#556B2F] cursor-pointer">Login</Link>
+                                    <Link to={'/login'} className="px-4 pt-0.5 pb-0.75 rounded-full bg-[#F7F3E9] text-[#556B2F] cursor-pointer">Login</Link>
                                 </ReactTooltip>
                             )
                         }
